b0c0953dac5118988f6add8c74c98498b696230b
ioborin22 <ioborin22@gmail.com>
2024-12-04 12:40:21 -0500

    20241204

--- js/google-map.js
+++ js/google-map.js
@@ -189,19 +189,31 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['New York'];
+    var addresses = ['5331 Dugdale Rd, Jacksonville, FL, 32210'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+      $.getJSON(
+        'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+          encodeURIComponent(addresses[x]) +
+          '&sensor=false',
+        null,
+        function (data) {
+          if (data.results && data.results.length > 0) {
+            var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
+              position: latlng,
+              map: map,
+              icon: 'images/loc.png', // Убедитесь, что файл `loc.png` существует
             });
-
-        });
+          } else {
+            console.error(
+              'Geocode was not successful for the following reason: ' +
+                data.status,
+            );
+          }
+        },
+      );
     }
     
 }
